78602f7665851e13e678a823259c4d91c94cf821
tomoaki-saito <like_kendo_0921@yahoo.co.jp>
2015-07-28 17:30:04 +0900

    Initial Commit

---
+++ barcode-reader-sample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  barcode-reader-sample
+//
+//  Created by tomoaki saito on 2015/07/28.
+//  Copyright (c) 2015年 tomoaki. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
